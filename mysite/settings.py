e5ba1a891de2de5b952bc887dad24d953a47b0c8
naivete13sky <chen320821@163.com>
2023-10-27 16:42:11 +0800

    2

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mysite',        
+        'USER': 'postgres',
+        'PASSWORD': 'cc',        
+        'HOST': '192.168.0.23',
+        'PORT': '5432',
     }
 }
 
