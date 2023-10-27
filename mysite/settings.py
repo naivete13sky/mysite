c40d8fe1907abe40b0ffda52499a17fbfdd93ea5
naivete13sky <chen320821@163.com>
2023-10-27 17:37:50 +0800

    static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,18 +26,21 @@ SECRET_KEY = 'django-insecure-7k27)&d89*=quz(n5m+k=cjvt$*)5akuo^rhg1tyww4)ybk^(l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -126,9 +130,36 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+#当运行 python manage.py collectstatic 的时候
+#STATIC_ROOT文件夹是用来将所有STATICRTFILES_DIRS中所有文件夹中的文件，以及各app中static中的文件都复制过来
+#把这些文件放在一起，是为了用apache等部署的时候方便使用
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+
+#其它存放静态文件的文件夹，可以用来存放项目中共用的静态文件，里面不能包含STATIC_ROOT
+#如果不想使用STATICFILES_DIRS 可以不用，都放在app里的static中也可以
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "common_static"),
+    #'/path/to/others/static/',  # 用不到的时候可以不写这一行
+)
+
+
+# 这个是默认设置，Django 默认会在 STATICFILES_DIRS中的文件夹 和 各app下的static文件夹中找文件
+# 注意有先后顺序，找到了就不再继续找了
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
+)
+
+
+
+
